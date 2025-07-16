53daf212a1be2c0ea9b35e9139fb7730b099b17f
Julien Dehos <dehos@univ-littoral.fr>
2025-07-16 12:16:03 +0200

    get images

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import CMark
+import Control.Monad (forM_)
+import Data.List (foldl')
+import Data.Text qualified as T
+import Data.Text.IO qualified as T
 import Prelude hiding (FilePath)
 import System.FilePath (normalise)
 import Turtle
@@ -24,14 +29,30 @@ getFilesSvg :: FilePath -> IO [FilePath]
 getFilesSvg dir = fmap changeExt <$> getFiles dir
   where changeExt file = dropExtension file <.> "svg"
 
+nodeToImages :: Node -> [FilePath]
+nodeToImages = go []
+  where
+    go acc (Node _ (IMAGE url _) ns) = normalise (T.unpack url) : foldl' go acc ns
+    go acc (Node _ _ ns) = foldl' go acc ns
+
 main :: IO ()
 main = do
   basedir <- options "Check images in markdown files" parser
   cd basedir
 
-  getMds >>= mapM_ print
-  getFiles "files" >>= mapM_ print
-  getFilesSvg "dot" >>= mapM_ print
-  getFilesSvg "tex" >>= mapM_ print
-  getFilesSvg "uml" >>= mapM_ print
+  mds <- getMds
+  files <- getFiles "files"
+  dots <- getFilesSvg "dot"
+  tex <- getFilesSvg "tex"
+  umls <- getFilesSvg "uml"
+
+  forM_ mds $ \md -> do
+    print md
+    input <- T.readFile md
+    let node = commonmarkToNode [] input
+        imgs = nodeToImages node
+    mapM_ putStrLn imgs
+
+  
+  pure ()
 
