ed5f728277ddd3441fa46b9edef4d2fd6ccbbf3a
Julien Dehos <dehos@univ-littoral.fr>
2025-07-16 11:29:37 +0200

    getfiles

--- src/Main.hs
+++ src/Main.hs
@@ -1,27 +1,37 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Prelude hiding (FilePath)
+import System.FilePath (normalise)
 import Turtle
 
 parser :: Parser FilePath
 parser = argPath "basedir"  "Base directory to check"
 
-checkdir :: FilePath -> IO ()
-checkdir dir = do
+shellToList :: Fold a [a]
+shellToList = Fold (\ys y -> ys++[y]) [] id
+
+getMds :: IO [FilePath]
+getMds = reduce shellToList (normalise <$> find (ends ".md") ".")
+
+getFiles :: FilePath -> IO [FilePath]
+getFiles dir = do
   exists <- testdir dir
-  when exists $ do
-    view (ls dir)
+  if exists
+  then reduce shellToList (normalise <$> ls dir)
+  else pure []
+
+getFilesSvg :: FilePath -> IO [FilePath]
+getFilesSvg dir = fmap changeExt <$> getFiles dir
+  where changeExt file = dropExtension file <.> "svg"
 
 main :: IO ()
 main = do
-    basedir <- options "Check images in markdown files" parser
-    cd basedir
+  basedir <- options "Check images in markdown files" parser
+  cd basedir
 
-    view (find (ends ".md") "." )
-
-    view (ls "./files/" )
-
-    checkdir "uml"
-    checkdir "dot"
-    checkdir "tex"
+  getMds >>= mapM_ print
+  getFiles "files" >>= mapM_ print
+  getFilesSvg "dot" >>= mapM_ print
+  getFilesSvg "tex" >>= mapM_ print
+  getFilesSvg "uml" >>= mapM_ print
 
